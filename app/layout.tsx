1facf58307fd68233cf19343164f5ffa26a78465
Jiang WenHao <109291929+jwh12333@users.noreply.github.com>
2023-06-28 21:51:38 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import "./styles/highlight.scss";
 import { getClientConfig } from "./config/client";
 
 export const metadata = {
-  title: "ChatGPT Next Web",
+  title: "ChatGPT Next We1b",
   description: "Your personal ChatGPT Chat Bot.",
   viewport: {
     width: "device-width",
