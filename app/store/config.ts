9a27b04f9c70d86e15b2333d943ee70227fcb399
jwh12123 <138111629+jwh12123@users.noreply.github.com>
2023-07-02 20:16:28 +0800

    Update config.ts

--- app/store/config.ts
+++ app/store/config.ts
@@ -56,36 +56,8 @@ const ENABLE_GPT4 = true;
 
 export const ALL_MODELS = [
   {
-    name: "gpt-4",
-    available: ENABLE_GPT4,
-  },
-  {
-    name: "gpt-4-0314",
-    available: ENABLE_GPT4,
-  },
-  {
     name: "gpt-4-0613",
     available: ENABLE_GPT4,
-  },
-  {
-    name: "claude+",
-    available: ENABLE_GPT4,
-  },
-  {
-    name: "claude-instant-100k",
-    available: ENABLE_GPT4,
-  },
-  {
-    name: "sage",
-    available: ENABLE_GPT4,
-  },
-  {
-    name: "gpt-3.5-turbo",
-    available: true,
-  },
-  {
-    name: "gpt-3.5-turbo-0301",
-    available: true,
   },
   {
     name: "gpt-3.5-turbo-0613",
@@ -95,30 +67,7 @@ export const ALL_MODELS = [
     name: "gpt-3.5-turbo-16k",
     available: true,
   },
-  {
-    name: "gpt-3.5-turbo-16k-0613",
-    available: true,
-  },
-  {
-    name: "qwen-v1", // 通义千问
-    available: false,
-  },
-  {
-    name: "ernie", // 文心一言
-    available: false,
-  },
-  {
-    name: "spark", // 讯飞星火
-    available: false,
-  },
-  {
-    name: "llama", // llama
-    available: false,
-  },
-  {
-    name: "chatglm", // chatglm-6b
-    available: false,
-  },
+
 ] as const;
 
 export type ModelType = (typeof ALL_MODELS)[number]["name"];
